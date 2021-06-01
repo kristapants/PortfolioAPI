aa2ad159444b473c1f8af64c843fe37fb863b9c1
kristapants <peachykeen@gmail.com>
2021-05-31 21:16:45 -0500

    cleaning out comments, unused code from models, extra characters

--- src/api/education/education-model.js
+++ src/api/education/education-model.js
@@ -1,34 +1,11 @@
 const db = require("../../../data/db-config");
 
 module.exports = {
-  add,
   find,
-  findBy,
-  findById,
 };
 
 function find() {
   return db("education as e")
     .join("users as u")
     .select("u.username", "e.id", "e.completed", "e.accolade", "e.about");
-}
-
-function findBy(filter) {
-  return db("users as u")
-    .join("roles as r", "u.role", "=", "r.id")
-    .select("u.id", "u.username", "r.name as role", "u.password")
-    .where(filter);
-}
-
-async function add(user) {
-  const [id] = await db("users").insert(user, "id");
-  return findById(id);
-}
-
-function findById(id) {
-  return db("users as u")
-    .join("roles as r", "u.role", "=", "r.id")
-    .select("u.id", "u.username", "r.name as role")
-    .where("u.id", id)
-    .first();
 }
